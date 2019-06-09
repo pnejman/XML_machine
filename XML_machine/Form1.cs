eb98d27fe51c13c181ff1468577b12b7bc3f53d0
pnejman <pnejman@BYDDTPENEJMAN01>
2019-06-09 17:23:17 +0200

    Program z 9.06

--- XML_machine/Form1.cs
+++ XML_machine/Form1.cs
@@ -6,24 +6,47 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Windows.Forms;
 
 namespace XML_machine
 {
     public partial class Form1 : Form
     {
+        OpenFileDialog opener = new OpenFileDialog(); //create open file object
+        
         public Form1()
         {
             InitializeComponent();
-            OpenFileDialog opener = new OpenFileDialog();
+            this.Text = "XML Machine";
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("1");
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            opener.
+            if (opener.ShowDialog() == DialogResult.OK) //display browse window, and if succes then:
+            {
+                textBox1.Text = opener.FileName; //diplay file name
+                button2.Enabled = true;
+            }
+        }
 
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            using (XmlTextReader reader = new XmlTextReader(opener.FileName)) //open file by its name. Using = well be dosposed after being used
+            {
+                try //check if file is valid
+                {
+                    while (reader.Read()) //read the whole file
+                    {
+
+                    }
+                }
+
+                catch //file is not valid
+                {
+                    MessageBox.Show("Error. Invalid XML file.");
+                }
+            }
         }
     }
 }
