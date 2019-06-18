0ea5c48b40d302d869400e92fb96525e4dc96a10
PN <pn@nomail.com>
2019-06-18 21:14:44 +0200

    Fixes. Ver 1.1.

--- XML_machine/Form1.Designer.cs
+++ XML_machine/Form1.Designer.cs
@@ -38,6 +38,8 @@
             this.calculate_button = new System.Windows.Forms.Button();
             this.my_console = new System.Windows.Forms.TextBox();
             this.clear_button = new System.Windows.Forms.Button();
+            this.title_label = new System.Windows.Forms.Label();
+            this.progress_bar = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -99,7 +101,6 @@
             // 
             this.counter_box.Location = new System.Drawing.Point(472, 95);
             this.counter_box.Name = "counter_box";
-            this.counter_box.ReadOnly = true;
             this.counter_box.Size = new System.Drawing.Size(100, 20);
             this.counter_box.TabIndex = 5;
             this.counter_box.TextChanged += new System.EventHandler(this.Enable_Second_Button);
@@ -135,11 +136,32 @@
             this.clear_button.UseVisualStyleBackColor = true;
             this.clear_button.Click += new System.EventHandler(this.Clear_button_Click);
             // 
+            // title_label
+            // 
+            this.title_label.AutoSize = true;
+            this.title_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.title_label.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.title_label.Location = new System.Drawing.Point(412, 9);
+            this.title_label.Name = "title_label";
+            this.title_label.Size = new System.Drawing.Size(160, 20);
+            this.title_label.TabIndex = 9;
+            this.title_label.Text = "XML Machine v.1.1";
+            // 
+            // progress_bar
+            // 
+            this.progress_bar.Location = new System.Drawing.Point(21, 454);
+            this.progress_bar.Name = "progress_bar";
+            this.progress_bar.Size = new System.Drawing.Size(551, 21);
+            this.progress_bar.TabIndex = 10;
+            this.progress_bar.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(595, 450);
+            this.ClientSize = new System.Drawing.Size(595, 489);
+            this.Controls.Add(this.progress_bar);
+            this.Controls.Add(this.title_label);
             this.Controls.Add(this.clear_button);
             this.Controls.Add(this.my_console);
             this.Controls.Add(this.calculate_button);
@@ -168,6 +190,8 @@
         private System.Windows.Forms.Button calculate_button;
         private System.Windows.Forms.TextBox my_console;
         private System.Windows.Forms.Button clear_button;
+        private System.Windows.Forms.Label title_label;
+        private System.Windows.Forms.ProgressBar progress_bar;
     }
 }
 
--- XML_machine/Form1.cs
+++ XML_machine/Form1.cs
@@ -6,39 +6,44 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml; //required to read XMLs <-- too obvious to comment (don't add comments like that)
+using System.Xml; //required to read XMLs [B]<-- too obvious to comment (don't add comments like that)
 using System.Windows.Forms;
-using System.IO; //required to save text to file <-- too obvious to comment (don't add comments like that)
+using System.IO; //required to save text to file [B]-- too obvious to comment (don't add comments like that)
 
 namespace XML_machine
 {
-
-
     public partial class Form1 : Form
     {
-        OpenFileDialog opener = new OpenFileDialog(); //create "open file" object <-- too obvious to comment (don't add comments like that)
+        OpenFileDialog opener = new OpenFileDialog(); //create "open file" object [B]<-- too obvious to comment (don't add comments like that)
+
+        //Global (class level) constructors:
+        string log_file = "";
+        string error_log_file = "";
 
         public Form1()
         {
             InitializeComponent();
-            this.Text = "XML Machine"; //change app title  <-- too obvious to comment (don't add comments like that) -- it applies to other comments as well.
+            this.Text = "XML Machine"; //change app title  [B]<-- too obvious to comment (don't add comments like that) -- it applies to other comments as well.
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle; //no resizing
+            this.MaximizeBox = false; //no maximizing
 
-            File.WriteAllText("log.txt", ""); //clear log files <-- sorry, not a good idea for managing logs
-            File.WriteAllText("error_log.txt", "");
+            DateTime program_start = DateTime.Now;
+            log_file = program_start.ToString("yyyyMMdd_HHmmss") + "_log.txt"; //generate names for log files from time of program launch
+            error_log_file = program_start.ToString("yyyyMMdd_HHmmss") + "_error_log.txt";
         }
 
-        private void Display(string to_display) //alias to write things in console  <-- the method name should be named to something that is obvious enough without the comment
+        private void DisplayConsole(string console_content) //alias to write things in console [B]<-- the method name should be named to something that is obvious enough without the comment
         {
-            my_console.Text += to_display;
-            File.AppendAllText("log.txt", to_display);
+            my_console.Text += console_content;
+            File.AppendAllText(log_file, console_content);
         }
 
-        private void DisplayError(string to_display) //alias to display errors <-- the method name should be named to something that is obvious enough without the comment
+        private void DisplayError(string error_msg) //alias to display errors [B]<-- the method name should be named to something that is obvious enough without the comment
         {
-            my_console.Text += to_display;
-            File.AppendAllText("error_log.txt", to_display);
-            MessageBox.Show(to_display);
-
+            my_console.Text += error_msg;
+            File.AppendAllText(error_log_file, error_msg);
+            File.AppendAllText(log_file, error_msg);
+            MessageBox.Show(error_msg);
         }
 
         private void Button1_Click(object sender, EventArgs e) //browse button clicked 
@@ -49,27 +54,50 @@ namespace XML_machine
             }
         }
 
-        private void Enable_Second_Button(object sender, EventArgs e) //enable second button
+        private void Enable_Second_Button(object sender, EventArgs e) //enable second button. Triggered by changing value of filename, operation number or operation type.
         {
-            if ((file_path.Text != "") && (operation_selector.Text != "")) //if none of required textboxes is blank
+            if ((file_path.Text != "") && (operation_selector.Text != "") && (counter_box.Text != "")) //if none of required textboxes is blank
             {
                 calculate_button.Enabled = true; //user can proceed with calculation
             }
+            else
+            {
+                calculate_button.Enabled = false;
+            }
         }
 
-        private void Button2_Click(object sender, EventArgs e) //calculate button clicked - <-- this method is too long (it contains too much processing, too many 'logical operations')
-        //as a rule of thumb, if a method is too long to fit on screen, split it into smaller methods
+        private void Clear_button_Click(object sender, EventArgs e) //clear console
         {
-            Display("Opening file:\r\n" + file_path.Text + "\r\n"); //display msg
-            int value_elements = 0; //counter to count number of "value" elements
-            List<int> imported_a = new List<int>(); //list of A values
-            List<int> imported_b = new List<int>(); //list of B values
+            my_console.Text = "";
+        }
 
-            using (XmlTextReader reader = new XmlTextReader(opener.FileName)) //open file by its name. "Using" means this object well be disposed after being used
+        //global (class level) contructors:
+        int no_of_values = 0; //number of <value> elements, read from XML
+        int no_of_operations = 0; //number of operations to perform, given by User
+        List<int> imported_a = new List<int>(); //list of A values
+        List<int> imported_b = new List<int>(); //list of B values
+
+        private void Open_and_read() //as the name suggest, this method is to open and read file. It also launches "calculate" method.
+        {
+            DisplayConsole($"Opening file:\r\n{file_path.Text}\r\n"); //display msg
+            
+            try
             {
-                try //watch for errors
+                no_of_operations = Int32.Parse(counter_box.Text); //counter to count number of "value" elements
+            }
+            catch //error of parsing counter_box int integer. Probably not a number.
+            {
+                DisplayError("Error while setting up operations counter. Please input valid integer number.\r\n");
+                return; //stop this function
+            }
+            DisplayConsole("Opening complete.\r\n");
+
+            using (XmlTextReader reader = new XmlTextReader(opener.FileName)) //open file by its name. "Using" means this object will be disposed after being used
+            {
+                try
                 {
-                    Display("Opening complete.\r\n----------\r\nReading values...\r\n");
+                    DisplayConsole("----------\r\n" +
+                                   "Reading values...\r\n");
 
                     while (reader.Read()) //count number of "Value" elements
                     {
@@ -77,103 +105,140 @@ namespace XML_machine
                         {
                             imported_a.Add(Int32.Parse(reader.GetAttribute("a"))); //read values, parse them to Integer, and save to lists
                             imported_b.Add(Int32.Parse(reader.GetAttribute("b")));
-                            Display("Value (" + value_elements + ")\r\n\ta: " + reader.GetAttribute("a") + "\r\n\tb: " + reader.GetAttribute("b") + "\r\n");
-                            value_elements++;
+                            DisplayConsole($"Value ({no_of_values})\r\n\ta: {reader.GetAttribute("a")}\r\n\tb: {reader.GetAttribute("b")}\r\n");
+                            no_of_values++;
                         }
                     }
-
-                    Display("Number of 'Value' elements detected: " + value_elements + "\r\n");
-                    counter_box.Text = "" + value_elements;
                 }
                 catch //error detected
                 {
-                    //it makes sense to get some details on the error - what went wrong? Invalid attribute value? Which attribute? No attribute? 
-                    DisplayError("Error while processing XML file.\r\n");
+                    //[B]it makes sense to get some details on the error - what went wrong? Invalid attribute value? Which attribute? No attribute? 
+                    DisplayError("Error while reading values from XML file. Please provide valid XML file with valid 'a' and 'b' parameters.\r\n");
                     return; //stop this function
                 }
             }
+            DisplayConsole("Number of Value elements detected: " + no_of_values + "\r\n");
 
-            Display("----------\r\nCalculating...\r\n");
-            //instead of j, k etc you can use better names for the iterator indexing variables (something more obvious for the reader)
-            for (int j = 0; j < value_elements; j++) //calculate as many times as there are "value" elements
+            //setup progress bar
+            progress_bar.Minimum = 0;
+            progress_bar.Maximum = no_of_values * no_of_operations;
+            progress_bar.Value = 0;
+            progress_bar.Step = 1;
+            Calculate();
+        }
+             
+        private void Calculate()
+        {
+            DisplayConsole("----------\r\n" +
+                           "Calculating...\r\n");
+            progress_bar.Visible = true;
+
+            //[B]instead of j, k etc you can use better names for the iterator indexing variables (something more obvious for the reader)
+            for (int current_value = 0; current_value < no_of_values; current_value++) //calculate as many times as there are "value" elements
             {
-                int result = imported_b[j]; //reset result
+                int result = imported_b[current_value]; //reset result
 
-                //use string interpolation rather than concatenation. It reads better:)
-                Display("Value (" + j + ")\r\n\tA = " + imported_a[j] +", B = "+ imported_b[j] + ", Operation = " + operation_selector.Text +", Number of operations = "+value_elements+"\r\n"); //display things
+                //[B]use string interpolation rather than concatenation. It reads better:)
+                DisplayConsole($"Value ({current_value})\r\n" +
+                               $"\tA = {imported_a[current_value]}, " +
+                               $"B = {imported_b[current_value]}, " +
+                               $"Operation = {operation_selector.Text}, " +
+                               $"Number of operations = {no_of_operations}\r\n"); //display things
 
                 switch (operation_selector.Text)
                 {
-                    case "Multiply": 
+                    case "Multiply":
                         try
                         {
-                            for (int k = 0; k < value_elements; k++)
+                            for (int current_operation = 0; current_operation < no_of_operations; current_operation++)
                             {
-                                Display("\t" + imported_a[j] + " * " + result + " = " + (imported_a[j] * result) + "\r\n");
-                                result = imported_a[j] * result;
+                                DisplayConsole($"\t{imported_a[current_value]} * {result} = {(imported_a[current_value] * result)}\r\n");
+                                result = imported_a[current_value] * result;
+                                progress_bar.PerformStep();
                             }
                         }
                         catch
                         {
                             DisplayError("Error while multiplying.\r\n");
+                            return;
                         }
                         break;
 
                     case "Divide":
                         try
                         {
-                            double f_result = Convert.ToDouble(imported_b[j]);
-                            for (int k = 0; k < value_elements; k++)
+                            double f_result = Convert.ToDouble(imported_b[current_value]);
+                            for (int current_operation = 0; current_operation < no_of_operations; current_operation++)
                             {
-                                Display("\t" + Convert.ToDouble(imported_a[j]) + " / " + f_result + " = " + (Convert.ToDouble(imported_a[j]) / f_result) + "\r\n");
-                                f_result = Convert.ToDouble(imported_a[j]) / f_result;
+                                DisplayConsole($"\t{Convert.ToDouble(imported_a[current_value])} / {f_result} =  {(Convert.ToDouble(imported_a[current_value]) / f_result)}\r\n");
+                                f_result = Convert.ToDouble(imported_a[current_value]) / f_result;
+                                progress_bar.PerformStep();
                             }
                         }
                         catch
                         {
                             DisplayError("Error while dividing.\r\n");
+                            return;
                         }
                         break;
 
                     case "Power":
                         try
                         {
-                            double d_result = Convert.ToDouble(imported_b[j]);
-                            for (int k = 0; k < value_elements; k++)
+                            double d_result = Convert.ToDouble(imported_b[current_value]);
+                            for (int current_operation = 0; current_operation < no_of_operations; current_operation++)
                             {
-                                Display("\t" + imported_a[j] + " ^ " + d_result + " = " + Math.Pow(Convert.ToDouble(imported_a[j]), d_result) + "\r\n");
-                                d_result = Math.Pow(Convert.ToDouble(imported_a[j]), d_result);
+                                DisplayConsole($"\t{imported_a[current_value]} ^ {d_result} = {Math.Pow(Convert.ToDouble(imported_a[current_value]), d_result)}\r\n");
+                                d_result = Math.Pow(Convert.ToDouble(imported_a[current_value]), d_result);
+                                progress_bar.PerformStep();
                             }
                         }
                         catch
                         {
                             DisplayError("Error while powering.\r\n");
+                            return;
                         }
                         break;
 
                     case "Substract":
                         try
                         {
-                            for (int k = 0; k < value_elements; k++)
+                            for (int current_operation = 0; current_operation < no_of_operations; current_operation++)
                             {
-                                Display("\t" + imported_a[j] + " - " + result + " = " + (imported_a[j] - result) + "\r\n");
-                                result = imported_a[j] - result;
+                                DisplayConsole($"\t{imported_a[current_value]} - {result} = {(imported_a[current_value] - result)}\r\n");
+                                result = imported_a[current_value] - result;
+                                progress_bar.PerformStep();
                             }
                         }
                         catch
                         {
                             DisplayError("Error while substracting.\r\n");
+                            return;
                         }
                         break;
+
+                    default:
+                        DisplayError("Error while initializing operation. Please select valid operation from the list.\r\n");
+                        return; //by default "break" is needed in every case, including "default". However if "return" is used (which technically makes "break" unreachable) adding "break" is not necessary.
                 }
             }
-            Display("Calculation complete\r\n----------\r\n");
+            DisplayConsole("Calculation complete\r\n" +
+                           "----------\r\n");
+            MessageBox.Show("Job finished!");
+            //byebye progress bar
+            progress_bar.Value = 0;
+            progress_bar.Visible = false;
         }
 
-        private void Clear_button_Click(object sender, EventArgs e) //clear console
+        private void Button2_Click(object sender, EventArgs e) //calculate button clicked [B]<-- this method is too long (it contains too much processing, too many 'logical operations')
+        //[B]as a rule of thumb, if a method is too long to fit on screen, split it into smaller methods
         {
-            my_console.Text = "";
-            Display("Console screen has been cleared.\r\n"); //oh, really??
+            no_of_values = 0; //reset number of <value> elements
+            no_of_operations = 0; //reset number of operations to perform
+            imported_a = new List<int>(); //reset list of A values
+            imported_b = new List<int>(); //reset list of B values
+
+            Open_and_read();
         }
     }
 }
